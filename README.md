a748bcca468ff85739af36b812f0a23265bdc56d
andrew <j.andregon15@gmail.com>
2025-04-06 17:49:35 +0200

    Add `isaaclab_dynamics` module and improve pendulum simulation
    Introduced `isaaclab_dynamics` package for control logic and system dynamics, including a keyboard-based controller. Refactored and expanded pendulum simulation scripts to leverage the new package, improving modularity and usability.

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ To train an environments:
 
 ## Information
 - To create an environment, a .usd file must be created from a .urdf
-- The .usd files from this project are to be found at `source/isaaclab_assets/isaaclab_assets/custom`. The .py files setup the sim simulation.
+- The .usd files from this project are to be found at `source/isaaclab_assets/isaaclab_assets/custom`. The .py files set up the simulation.
 - The task definitions (RL-problem) are defined in `source/isaaclab_tasks/isaaclab_tasks`. Inside there are the
   managed tasks and the direct tasks. Managed tasks use higher level abstractions that reduce the difficulty while
   direct tasks allow granular control.
--- scripts/codesign/run_pendulum_keyboard.py
+++ scripts/codesign/run_pendulum_keyboard.py
@@ -26,13 +26,16 @@ simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
-import math
 import torch
+
+from isaaclab_dynamics import controllers
 
 from isaaclab.envs import ManagerBasedRLEnv
 
 # from isaaclab_tasks.manager_based.classic.cartpole.cartpole_env_cfg import CartpoleEnvCfg
 from isaaclab_tasks.manager_based.custom.pendulum_grav_simple.pendulum_grav_simple_env_cfg import PendulumEnvCfg
+
+controller = controllers.Controller()
 
 
 def main():
@@ -40,6 +43,7 @@ def main():
     # create environment configuration
     env_cfg = PendulumEnvCfg()
     env_cfg.scene.num_envs = args_cli.num_envs
+
     # setup RL environment
     env = ManagerBasedRLEnv(cfg=env_cfg)
 
@@ -47,18 +51,23 @@ def main():
     count = 0
     while simulation_app.is_running():
         with torch.inference_mode():
+
             # reset
             if count % 300 == 0:
                 count = 0
                 env.reset()
                 print("-" * 80)
                 print("[INFO]: Resetting environment...")
+
             # sample random actions
-            joint_efforts = torch.randn_like(env.action_manager.action)
+            # joint_efforts = torch.randn_like(env.action_manager.action)
+            joint_efforts = torch.tensor([[controller.step()]])
+            # print(f"Joint effort: {value}")
+
             # step the environment
             obs, rew, terminated, truncated, info = env.step(joint_efforts)
             # print current orientation of pole
-            print("[Env 0]: Pendulum joint: ", math.degrees(obs["policy"][0][0].item()))
+            # print("[Env 0]: Pendulum joint: ", math.degrees(obs["policy"][0][0].item()))
             # update counter
             count += 1
 
